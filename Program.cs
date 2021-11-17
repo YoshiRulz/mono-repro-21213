9c6b995d0820523d532fbd6cdb73281100c5acc0
YoshiRulz <OSSYoshiRulz@gmail.com>
2021-11-18 00:32:42 +1000

    new

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
 using System;
 using System.Windows.Forms;
 
+#if true
+// new, more troubling repro
+
+try {
+	FolderBrowserDialog fbd = new();
+	Console.WriteLine("showing FBD...");
+	fbd.ShowDialog();
+	Console.WriteLine(fbd.FileName);
+} catch (Exception e) {
+	Console.WriteLine($"caught {e}");
+}
+
+#else
+// initial repro
+
 try {
 	OpenFileDialog ofd = new();
 	Console.WriteLine("showing OFD...");
@@ -9,3 +24,5 @@ try {
 } catch (Exception e) {
 	Console.WriteLine($"caught {e}");
 }
+
+#endif
